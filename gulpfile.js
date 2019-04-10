b24474ca53e67f58ddfe15c93cc1e5f39973e9ab
Nuzairb <nuzairb@gmail.com>
2019-04-10 20:50:54 +0500

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,92 +1,143 @@
-var gulp = require("gulp"),
-    sass = require("gulp-sass"),
-    postcss = require("gulp-postcss"),
-    autoprefixer = require("autoprefixer"),
-    cssnano = require("cssnano"),
-    sourcemaps = require("gulp-sourcemaps"),
-    browserSync = require("browser-sync").create();
+/**
+ * A simple Gulp 4 Starter Kit for modern web development.
+ *
+ * @package @jr-cologne/create-gulp-starter-kit
+ * @author JR Cologne <kontakt@jr-cologne.de>
+ * @copyright 2019 JR Cologne
+ * @license https://github.com/jr-cologne/gulp-starter-kit/blob/master/LICENSE MIT
+ * @version v0.5.0-alpha
+ * @link https://github.com/jr-cologne/gulp-starter-kit GitHub Repository
+ * @link https://www.npmjs.com/package/@jr-cologne/create-gulp-starter-kit npm package site
+ *
+ * ________________________________________________________________________________
+ *
+ * gulpfile.js
+ *
+ * The gulp configuration file.
+ *
+ */
 
-var paths = {
-    styles: {
-        // By using styles/**/*.sass we're telling gulp to check all folders for any sass file
-        src: "src/scss/*.scss",
-        // Compiled files will end up in whichever folder it's found in (partials are not compiled)
-        dest: "src/css"
-    }
- 
-    // Easily add additional paths
-    // ,html: {
-    //  src: '...',
-    //  dest: '...'
-    // }
-};
+const gulp                      = require('gulp'),
+      del                       = require('del'),
+      sourcemaps                = require('gulp-sourcemaps'),
+      plumber                   = require('gulp-plumber'),
+      sass                      = require('gulp-sass'),
+      autoprefixer              = require('gulp-autoprefixer'),
+      cssnano                   = require('gulp-cssnano'),
+      babel                     = require('gulp-babel'),
+      webpack                   = require('webpack-stream'),
+      uglify                    = require('gulp-uglify'),
+      concat                    = require('gulp-concat'),
+      imagemin                  = require('gulp-imagemin'),
+      browserSync               = require('browser-sync').create(),
 
-function style() {
-    return gulp
-        .src(paths.styles.src)
-        // Initialize sourcemaps before compilation starts
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .on("error", sass.logError)
-        // Use postcss with autoprefixer and compress the compiled file using cssnano
-        .pipe(postcss([autoprefixer(), cssnano()]))
-        // Now add/write the sourcemaps
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(paths.styles.dest))
-        // Add browsersync stream pipe after compilation
-        .pipe(browserSync.stream());
-}
+      src_folder                = './src/',
+      src_assets_folder         = src_folder + 'assets/',
+      dist_folder               = './dist/',
+      dist_assets_folder        = dist_folder + 'assets/',
+      node_modules_folder       = './node_modules/',
+      dist_node_modules_folder  = dist_folder + 'node_modules/',
 
+      node_dependencies         = Object.keys(require('./package.json').dependencies || {});
 
+gulp.task('clear', () => del([ dist_folder ]));
 
+gulp.task('html', () => {
+  return gulp.src([ src_folder + '**/*.html' ], { base: src_folder })
+    .pipe(gulp.dest(dist_folder))
+    .pipe(browserSync.stream());
+});
 
-// A simple task to reload the page
-function reload(done) {
-    browserSync.reload();
-    done();
-}
+gulp.task('sass', () => {
+  return gulp.src([
+    src_assets_folder + 'sass/**/*.sass',
+    src_assets_folder + 'scss/**/*.scss'
+  ])
+    .pipe(sourcemaps.init())
+      .pipe(plumber())
+      .pipe(sass())
+      .pipe(autoprefixer({
+        browsers: [ 'last 3 versions', '> 0.5%' ]
+      }))
+      .pipe(cssnano())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(dist_assets_folder + 'css'))
+    .pipe(browserSync.stream());
+});
 
-// Add browsersync initialization at the start of the watch task
-function watch() {
-    browserSync.init({
-        // You can tell browserSync to use this directory and serve it as a mini-server
-        server: {
-            baseDir: "./src"
-        }
-        // If you are already serving your website locally using something like apache
-        // You can use the proxy setting to proxy that instead
-        // proxy: "yourlocal.dev"
+gulp.task('js', () => {
+  return gulp.src([ src_assets_folder + 'js/**/*.js' ])
+    .pipe(plumber())
+    .pipe(webpack({
+      mode: 'production'
+    }))
+    .pipe(sourcemaps.init())
+      .pipe(babel({
+        presets: [ '@babel/env' ]
+      }))
+      .pipe(concat('all.js'))
+      .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(dist_assets_folder + 'js'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('images', () => {
+  return gulp.src([ src_assets_folder + 'images/**/*.+(png|jpg|jpeg|gif|svg|ico)' ])
+    .pipe(plumber())
+    .pipe(imagemin())
+    .pipe(gulp.dest(dist_assets_folder + 'images'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('vendor', () => {
+  if (node_dependencies.length === 0) {
+    return new Promise((resolve) => {
+      console.log("No dependencies specified");
+      resolve();
     });
-    gulp.watch(paths.styles.src, style);
-    // We should tell gulp which files to watch to trigger the reload
-    // This can be html or whatever you're using to develop your website
-    // Note -- you can obviously add the path to the Paths object
-    gulp.watch("src/*.html", reload);
-}
- 
-// We don't have to expose the reload function
-// It's currently only useful in other functions
+  }
 
-    
-// Don't forget to expose the task!
-exports.watch = watch
+  return gulp.src(node_dependencies.map(dependency => node_modules_folder + dependency + '/**/*.*'), { base: node_modules_folder })
+    .pipe(gulp.dest(dist_node_modules_folder))
+    .pipe(browserSync.stream());
+});
 
-// Expose the task by exporting it
-// This allows you to run it from the commandline using
-// $ gulp style
-exports.style = style;
+gulp.task('build', gulp.series('clear', 'html', 'sass', 'js', 'images', 'vendor'));
 
-/*
- * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
- */
-var build = gulp.parallel(style, watch);
- 
-/*
- * You can still use `gulp.task` to expose tasks
- */
-//gulp.task('build', build);
- 
-/*
- * Define default task that can be called by just running `gulp` from cli
- */
-gulp.task('default', build);
+gulp.task('dev', gulp.series('html', 'sass', 'js'));
+
+gulp.task('serve', () => {
+  return browserSync.init({
+    server: {
+      baseDir: [ 'dist' ],
+      port: 3000
+    },
+    open: false
+  });
+});
+
+gulp.task('watch', () => {
+  const watchImages = [
+    src_assets_folder + 'images/**/*.+(png|jpg|jpeg|gif|svg|ico)'
+  ];
+
+  const watchVendor = [];
+
+  node_dependencies.forEach(dependency => {
+    watchVendor.push(node_modules_folder + dependency + '/**/*.*');
+  });
+
+  const watch = [
+    src_folder + '**/*.html',
+    src_assets_folder + 'sass/**/*.sass',
+    src_assets_folder + 'scss/**/*.scss',
+    src_assets_folder + 'js/**/*.js'
+  ];
+
+  gulp.watch(watch, gulp.series('dev')).on('change', browserSync.reload);
+  gulp.watch(watchImages, gulp.series('images')).on('change', browserSync.reload);
+  gulp.watch(watchVendor, gulp.series('vendor')).on('change', browserSync.reload);
+});
+
+gulp.task('default', gulp.series('build', gulp.parallel('serve', 'watch')));
